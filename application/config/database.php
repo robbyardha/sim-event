f9909b7b03e7961ef19a2597dde4817c6a106bde
robbysmamita <69607537+robbysmamita@users.noreply.github.com>
2022-06-13 09:09:10 +0700

    host online database

--- application/config/database.php
+++ application/config/database.php
@@ -73,11 +73,33 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'sim-event',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '103.154.113.245',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'Ictsmamita18',
 	'database' => 'sim-event',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
